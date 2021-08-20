405633f9ab8b7167b5fd95854b997c376cdb5814
presto3000 <pawlowskikrzysztof3000@gmail.com>
2021-08-20 07:29:48 +0200

    Base Setup
    Base Setup

---
+++ Source/EndlessRunner/EndlessRunnerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EndlessRunnerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENDLESSRUNNER_API AEndlessRunnerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
